c42b3aaa2be9813985fcaf48c952b67ef78edc25
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2024-02-03 23:30:24 +0200

    Add validator

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,22 +6,28 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use App\Entity\Product;
 
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'create_product')]
-    public function createProduct(ManagerRegistry $doctrine): Response
+    public function createProduct(ManagerRegistry $doctrine, ValidatorInterface $validator): Response
     {
         $entityManager = $doctrine->getManager();
 
         $product = new Product();
-        $product->setName('Table');
-        $product->setPrice(599);
-        $product->setDescription('Strong and large');
+        $product->setName('Chair');
+        $product->setPrice(1200);
+        $product->setDescription('Comfortable');
 
         $entityManager->persist($product);
         $entityManager->flush();
+
+$errors = $validator->validate($product);
+if(count($errors) > 0){
+    return new Response((string) $errors, 400);
+}
 
         return new Response('Saved new product with id '.$product->getId());
     }
